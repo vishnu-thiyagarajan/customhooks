39480f485cedbb2d34a0494138e425e585150b32
VishnuSD <vishnu@smarterdharma.com>
2020-08-07 23:08:24 +0530

    uselocalstorage update

--- src/components/Name.js
+++ src/components/Name.js
@@ -3,13 +3,19 @@ import useLocalStorage from './useLocalStorage';
 import useUpdateLogger from './useUpdateLogger'; 
 
 export default function Name(){
-    const [name, setName] = useLocalStorage('name', '')
-    useUpdateLogger(name)
+    const [name, setName, removeKey, removeAll, getByIndex] = useLocalStorage('name', '')
+    useUpdateLogger(getByIndex(0))
     return(
+        <>
         <input
           type="text"
           value={name}
           onChange={(e)=>setName(e.target.value)}
         />
+        <br />
+        <button onClick={()=>{removeKey('name')}}>remove</button>
+        <br />
+        <button onClick={()=>{removeAll()}}>remove All</button>
+        </>
     )
 }
--- src/components/useLocalStorage/index.js
+++ src/components/useLocalStorage/index.js
@@ -11,8 +11,11 @@ export default function useLocalStorage(key, initialValue){
   const [value, setValue] = useState(()=>{
       return getSavedValue(key, initialValue)
   })
+  const removeKey = (key)=>localStorage.removeItem(key)
+  const clearAll = (key)=>localStorage.clear()
+  const getByIndex = (index)=>localStorage.key(index)
   useEffect(()=>{
       localStorage.setItem(key, JSON.stringify(value))
   }, [value,key])
-  return [value, setValue]
+  return [value, setValue, removeKey, clearAll, getByIndex]
 }
